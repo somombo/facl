123f21a6961b77bb949f7d1c1db277799febb806
Somo S <somo@mombo.solutions>
2018-09-01 21:49:16 -0400

    refactor: improving support for eventual context plugin API

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export default function (rules_source: string, js = false){
   const acl = compile(rules_source, js)
   const context = {}
   return (req: Request, res: Response, next: NextFunction) => {
+ 
 
     // TODO: add support for more request functionalities
     context["request"] = {
@@ -16,11 +17,34 @@ export default function (rules_source: string, js = false){
       accepts(str) {
         return !!req.accepts(str)
       },
-      auth: {
-        uid: 1234 // TODO: REMOVE THIS Somo, for test only!
-      }
+      is(str) {
 
+        return !!req.is(str)
+      },
+      auth: class {
+        static get uid() {
+          
+
+          const auth = req.headers.authorization
+          if (!auth){
+            throw new Error("No auth header available")
+          }
+          
+          const tokens = auth.split(" ")
+          if (!tokens || !tokens.length) {
+            throw new Error("Cannot parse auth token")
+          }
+
+          const authToken = tokens[ tokens.length - 1 ]
+
+          return  authToken
+        }
+      }
+      
+
+        
     }
+
 
     for (const rule of acl) {
       
@@ -30,13 +54,12 @@ export default function (rules_source: string, js = false){
         const actionAuthdAllows = rule.allows.filter( allow => /* action_matches */ allow.isAllowedAction(req.method) )
 
         if ( !actionAuthdAllows.length ) {
-          // res.status(401).json({ error: "Unauthorized Action", status: 401, time: Date.now()})
-          return next({ message: "Unauthorized Action", status: 401, time: Date.now()})
+          return res.status(401).json({ message: "Unauthorized Action", status: 401, time: Date.now()})
           
         }
 
         for (const allow of actionAuthdAllows) {
-
+          
           /* get condition decision */
           if (/*  condition is met */ allow.isAllowedReqIn(context) ) {
             return next()
@@ -46,15 +69,14 @@ export default function (rules_source: string, js = false){
         }  
 
         // is accessible resource, with authd action but did not meet a single auth condition
-        // res.status(401).json({ error: "Access denied", status: 401, time: Date.now()})
+        return  res.status(401).json({ message: "Access denied", status: 401, time: Date.now()})
         
-        return next({ message: "Access denied", status: 401, time: Date.now()})
+      
 
       }
     }
 
-    // res.status(401).json({ error: "Not Authorized", status: 401, time: Date.now()});
-    return next({ message: "Not Authorized", status: 401, time: Date.now()});
+    return res.status(401).json({ message: "Not Authorized", status: 401, time: Date.now()});
 
   }
 }
